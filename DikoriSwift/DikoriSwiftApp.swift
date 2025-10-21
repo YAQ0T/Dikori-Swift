c0a6b7a4902811a73b8f8f4e40554a979df4ca8c
Ahmad Salous <yaqot.1099@gmail.com>
2025-10-21 12:18:26 +0300

    Initial Commit

---
+++ DikoriSwift/DikoriSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DikoriSwiftApp.swift
+//  DikoriSwift
+//
+//  Created by Ahmad Salous on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct DikoriSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DikoriSwiftTests/DikoriSwiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  DikoriSwiftTests.swift
+//  DikoriSwiftTests
+//
+//  Created by Ahmad Salous on 21/10/2025.
+//
+
+import Testing
+
+struct DikoriSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
