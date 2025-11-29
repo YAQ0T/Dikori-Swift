11ca00df68acd31c99a6d1b6abba1bae66f1267a
Ahmad Salous <yaqot.1099@gmail.com>
2025-11-29 12:26:41 +0200

    remove android

--- Android/app/src/main/java/com/example/dikor_android/session/Session.kt
+++
@@ -1,8 +0,0 @@
-package com.example.dikor_android.session
-
-data class Session(
-    val accessToken: String,
-    val refreshToken: String,
-    val phoneNumber: String,
-    val isVerified: Boolean
-)
